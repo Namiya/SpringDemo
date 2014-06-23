e87c62e588f8454c921c6cd85259752503a24df4
Namiya <Namiya.Yuuji@gmail.com>
2014-06-23 18:57:56 -0400

    add "property" in spring.xml which bind with the private string type ini MessageBean.java

--- src/main/java/bean/MessageBean.java
+++ src/main/java/bean/MessageBean.java
@@ -1,9 +1,19 @@
 package bean;
 
 public class MessageBean {
+	private String type;
+	
 	public void PrintMessage()
 	{
-		System.out.println("hello world");
+		System.out.println(getType() + "hello world");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }

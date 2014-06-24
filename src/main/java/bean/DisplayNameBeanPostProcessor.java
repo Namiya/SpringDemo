149a99838ca474a68f120236c43acb9f29ca6803
Namiya <Namiya.Yuuji@gmail.com>
2014-06-24 16:59:55 -0400

    add the new files created in previous steps.

---
+++ src/main/java/bean/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("In After Initialization method. Bena name is " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("In Before Initialization method. Bena name is " + beanName);
+		return bean;
+	}
+
+}

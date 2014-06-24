4f940a46bb9a54173dbdc07b1846c6869bf8bbc6
Namiya <Namiya.Yuuji@gmail.com>
2014-06-23 20:39:40 -0400

    change "property" to "constructor-arg"

--- src/main/java/bean/MessageBean.java
+++ src/main/java/bean/MessageBean.java
@@ -2,18 +2,35 @@ package bean;
 
 public class MessageBean {
 	private String type;
+	private int number;
+
+	public int getNumber()
+	{
+		return number;
+	}
+	
+	public MessageBean(String type)
+	{
+		this.type = type;
+	}
+	
+	public MessageBean(String type, int number)
+	{
+		this.type = type;
+		this.number = number;
+	}
 	
 	public void PrintMessage()
 	{
-		System.out.println(getType() + "hello world");
+		System.out.println(getType() + "time " + getNumber() );
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+/*	public void setType(String type) {
 		this.type = type;
-	}
+	}*/
 
 }

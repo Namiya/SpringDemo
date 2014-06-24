5951ee30850a63b2a9b6839bd4ffd60dd352e7c7
Namiya <Namiya.Yuuji@gmail.com>
2014-06-23 21:27:34 -0400

    Inject objects (month, date)  into messagebean in spring.xml

--- src/main/java/bean/MessageBean.java
+++ src/main/java/bean/MessageBean.java
@@ -3,6 +3,24 @@ package bean;
 public class MessageBean {
 	private String type;
 	private int number;
+	private MessageContent month;
+	private MessageContent date;
+
+	public MessageContent getMonth() {
+		return month;
+	}
+
+	public void setMonth(MessageContent month) {
+		this.month = month;
+	}
+
+	public MessageContent getDate() {
+		return date;
+	}
+
+	public void setDate(MessageContent date) {
+		this.date = date;
+	}
 
 	public int getNumber()
 	{
@@ -22,12 +40,14 @@ public class MessageBean {
 	
 	public void PrintMessage()
 	{
-		System.out.println(getType() + "time " + getNumber() );
+		System.out.println(getType() + "time " + getNumber() + "/" + getMonth().getTime() + "/" + getDate().getTime());
 	}
 
 	public String getType() {
 		return type;
 	}
+
+
 
 /*	public void setType(String type) {
 		this.type = type;
---
+++ src/main/java/bean/MessageContent.java
@@ -0,0 +1,16 @@
+package bean;
+
+public class MessageContent {
+	private int time;
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+
+
+}
